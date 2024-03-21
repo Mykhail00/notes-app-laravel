70eeae5682f6fdd7dc8c6fb86763c8660c147e40
Mykhail00 <bazhenovm000@gmail.com>
2024-03-21 15:52:22 +0200

    added notes conroller and views

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Route::get('/note', [NoteController::class, 'index'])->name('note.index');
+//Route::get('/note/create', [NoteController::class, 'create'])->name('note.create');
+//Route::post('/note', [NoteController::class, 'store'])->name('note.store');
+//Route::get('/note/{id}', [NoteController::class, 'show'])->name('note.show');
+//Route::get('note/{id}/edit', [NoteController::class, 'edit'])->name('note.edit');
+//Route::put('/note/{id}', [NoteController::class, 'update'])->name('note.update');
+//Route::delete('/note/{id}', [NoteController::class, 'destroy']);
+
+Route::resource('note', NoteController::class);
